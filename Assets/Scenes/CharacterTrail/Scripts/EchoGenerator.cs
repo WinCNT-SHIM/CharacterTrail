9350d527b8edf46ca7c082485b9e8f41e8836205
WinCNT <34294605+WinCNT-SHIM@users.noreply.github.com>
2025-05-18 17:28:15 +0900

    Echo Color 기능 추가

--- Assets/Scenes/CharacterTrail/Scripts/EchoGenerator.cs
+++ Assets/Scenes/CharacterTrail/Scripts/EchoGenerator.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace SSS.System.Echo
 {
@@ -7,9 +8,24 @@ namespace SSS.System.Echo
     public class EchoGenerator : MonoBehaviour
     {
         public SkinnedMeshRenderer skinnedMeshRenderer; // 복사 대상
-        public Material ghostMaterial; // 잔상 표현용 머티리얼
+        public Material echoMaterial; // 잔상 표현용 머티리얼
         
         public GameObject echoPrefab;
+        
+        [Header("Echo Color")]
+        public Animator animator;
+        [SerializeField] private Color echoColor;
+        private static readonly int IsColorLoopOn = Animator.StringToHash("IsColorLoop");
+        private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+
+        void Start()
+        {
+            if (animator)
+            {
+                animator.SetBool(IsColorLoopOn, true);
+                // animator.Play("CharacterTrailColorLoop");
+            }
+        }
         
         public void CreateEcho(float echoDuration)
         {
@@ -20,7 +36,7 @@ namespace SSS.System.Echo
             Mesh bakedMesh = new Mesh();
             skinnedMeshRenderer.BakeMesh(bakedMesh);
 
-            // // 오브젝트 생성
+            // 오브젝트 생성
             var echo = Instantiate(echoPrefab);
             echo.transform.position = skinnedMeshRenderer.transform.position;
             echo.transform.rotation = skinnedMeshRenderer.transform.rotation;
@@ -33,9 +49,15 @@ namespace SSS.System.Echo
             mf.sharedMesh = bakedMesh;
 
             int subMeshCount = bakedMesh.subMeshCount;
-            // // 모든 SubMesh에 동일한 머티리얼 적용
-            var materials = Enumerable.Repeat(ghostMaterial, subMeshCount).ToArray();
+            // 모든 SubMesh에 동일한 머티리얼 적용
+            var materials = Enumerable.Repeat(echoMaterial, subMeshCount).ToArray();
             mr.sharedMaterials = materials;
+            
+            // 머티리얼 프로퍼티 블럭 설정
+            var mpb = new MaterialPropertyBlock();
+            mpb.SetColor(BaseColor, echoColor);
+            for (int i = 0; i < subMeshCount; ++i)
+                mr.SetPropertyBlock(mpb, i); // 서브메시 단위로 설정
 
             Destroy(echo, echoDuration);
         }
